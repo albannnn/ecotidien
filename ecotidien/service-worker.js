1e218b82d8946f0b9eaf6a8a16aad9d11e660045
albannnn <alban.monge56@gmail.com>
2023-02-05 17:44:33 +0100

    désactivation du service worker

--- ecotidien/service-worker.js
+++ ecotidien/service-worker.js
@@ -3,27 +3,4 @@ const assets = ['/', '/index.php', '/app.js','/styles.css'];
 
 
 
-//mettre en cache
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      cache.addAll(assets);
-    })
-  );
-});
-
-//fetcher le cache 
-
-self.addEventListener("fetch", (e) => {
-
- // console.log(e.request);
-
- e.respondWith(
-   caches.match(e.request).then((cache) => {
-     return cache || fetch(e.request);
-   })
- ) 
- 
-})
 
